e8ae472c2fca9ba352786c2521c388c90508b84c
cfek <kgomankov@gmail.com>
2021-09-24 18:22:56 +0100

    code refactoring

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import time
 
 # Setting up access tokens
 # spotify tokens
-CLIENT_ID = os.environ.get('spotify_client_id')
-CLIENT_SECRET = os.environ.get('spotify_client_secret')
+client_id = os.environ.get('spotify_client_id')
+client_secret = os.environ.get('spotify_client_secret')
 authorise_url = "https://accounts.spotify.com/authorize"
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
+token_url = 'https://accounts.spotify.com/api/token'
 # gcp tokens
 yt_id = os.environ.get('yt_id')
 yt_secret = os.environ.get('yt_secret')
@@ -18,41 +18,32 @@ yt_scopes = ["https://www.googleapis.com/auth/youtube"]
 # youtube data api v3 token
 yt_api_key = os.environ.get('yt_api_key')
 
+name_artist = []
+spotify_playlist_id=input()
 
 # Spotify authorisation
-def authorise():
+def spotify_authorise():
     token_url = "https://accounts.spotify.com/api/token"
     token_params = {"grant_type": "client_credentials"}
-    client_creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
+    client_creds = f"{client_id}:{client_secret}"
     headers = {"Authorization": f"Basic {base64.b64encode(client_creds.encode()).decode()}"}
     res = requests.post(token_url, data=token_params, headers=headers)
     return res.json()
 
-
+#get a list of strings consisting of the names of the track and the artist
 def get_playlist_tracks(access_token, playlist_id):
     playlist_url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
     headers = {"Authorization": f"Bearer {access_token}"}
     r = requests.get(playlist_url, headers=headers)
     tracks_items = r.json()["tracks"]["items"]
-    global name_artist
-    name_artist = []
     for track in tracks_items:
         name = track["track"]["name"] + ","
-
         artists = ""
-        # print("Track:", track["track"]["name"])
         for artist in track["track"]["artists"]:
-            # artists.append(artist["name"])
             artists += str(artist["name"]) + ","
-
         name_artist.append(name + artists)
 
-
-if __name__ == '__main__':
-    token = authorise()["access_token"]
-    get_playlist_tracks(token, "6T2nXNSsfN4tIBdfQGtBbc")
-
-
+#get first video for each search query and compile into one anonymous youtube playlist
 def get_youtube_playlist():
     playlist_link = "http://www.youtube.com/watch_videos?video_ids="
     for name in name_artist:
@@ -65,3 +56,11 @@ def get_youtube_playlist():
         playlist_link += response['items'][0]["id"]["videoId"] + ","
         time.sleep(1)
     return playlist_link
+
+if __name__ == '__main__':
+    token = spotify_authorise()["access_token"]
+    get_playlist_tracks(token, spotify_playlist_id)
+    print(get_youtube_playlist())
+
+
+
