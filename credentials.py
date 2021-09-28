8e88522391bc696bad39510ac0d4b2ff636e58f7
cfek <kgomankov@gmail.com>
2021-09-28 21:48:20 +0100

    added telegram bot integration  Pleas enter he commit message for your changes. Lines starting

---
+++ credentials.py
@@ -0,0 +1,3 @@
+bot_token = "2043997987:AAGlWAuU5KoIlmlajDmwdDE0My8M6DiNQR0"
+bot_user_name = "@Playlist_converter_bot"
+URL = "https://playlist--converter.herokuapp.com"
--- playlist_processing.py
+++ playlist_processing.py
@@ -1,8 +1,8 @@
-import os
-from googleapiclient.discovery import build
 import base64
-import requests
+import os
 import time
+import requests
+from googleapiclient.discovery import build
 
 # Setting up access tokens
 # spotify tokens
@@ -19,7 +19,9 @@ yt_scopes = ["https://www.googleapis.com/auth/youtube"]
 yt_api_key = os.environ.get('yt_api_key')
 
 name_artist = []
-spotify_playlist_id=input()
+spotify_playlist_id = "5PKZSKuHP4d27SXO5fB9Wl"
+
+
 
 # Spotify authorisation
 def spotify_authorise():
@@ -30,7 +32,8 @@ def spotify_authorise():
     res = requests.post(token_url, data=token_params, headers=headers)
     return res.json()
 
-#get a list of strings consisting of the names of the track and the artist
+
+# get a list of strings consisting of the names of the track and the artist
 def get_playlist_tracks(access_token, playlist_id):
     playlist_url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
     headers = {"Authorization": f"Bearer {access_token}"}
@@ -43,7 +46,8 @@ def get_playlist_tracks(access_token, playlist_id):
             artists += str(artist["name"]) + ","
         name_artist.append(name + artists)
 
-#get first video for each search query and compile into one anonymous youtube playlist
+
+# get first video for each search query and compile into one anonymous youtube playlist
 def get_youtube_playlist():
     playlist_link = "http://www.youtube.com/watch_videos?video_ids="
     for name in name_artist:
@@ -54,13 +58,12 @@ def get_youtube_playlist():
             maxResults=1)
         response = request.execute()
         playlist_link += response['items'][0]["id"]["videoId"] + ","
-        time.sleep(1)
+        time.sleep(0.1)
     return playlist_link
 
-if __name__ == '__main__':
+def spotify_to_youtube(spotify_playlist_id):
     token = spotify_authorise()["access_token"]
     get_playlist_tracks(token, spotify_playlist_id)
-    print(get_youtube_playlist())
-
+    return get_youtube_playlist()
 
 
